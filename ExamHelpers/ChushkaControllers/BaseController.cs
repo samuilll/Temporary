2796e304b9aa059090cd319c6fb544c8dbfe61ad
samuilll <samuil_dechev@abv.bg>
2018-11-03 14:25:12 +0200

    ...........

---
+++ ExamHelpers/ChushkaControllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using ChushkaWebApp.Data;
+using Microsoft.EntityFrameworkCore;
+using SIS.MvcFramework;
+
+namespace ChushkaWebApp.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        protected BaseController()
+        {
+            this.Db = new ApplicationDbContext();
+            Db.Database.Migrate();
+        }
+
+        public ApplicationDbContext Db { get; }
+    }
+}
---
+++ ExamHelpers/Validation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ChushkaWebApp.Utilities
+{
+   public class Validation
+    {
+        public static bool IsValid(object obj)
+        {
+            var validationContext = new ValidationContext(obj);
+
+            var result = new List<ValidationResult>();
+
+            return Validator.TryValidateObject(obj, validationContext, result, true);
+        }
+    }
+}
